56d81756412f8ae26b33e6de50606b639d3e64b4
Charith <vinodyav@tellko.com>
2023-04-08 18:14:06 +0530

    Removed Unwanted Code Lines

--- app/src/main/java/com/example/kuppiapp/UserAdapter.kt
+++ app/src/main/java/com/example/kuppiapp/UserAdapter.kt
@@ -5,9 +5,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kuppiapp.databinding.AdapterUserBinding
-import com.google.android.material.shadow.ShadowRenderer
 
-class UserAdapter(val context: Context, private var users: List<User>) :
+class UserAdapter(private val context: Context, private var users: List<User>) :
     RecyclerView.Adapter<MainViewHolder>(), DeleteButtonClickListener {
     //    var users = listOf<User>()
     fun setUserList(users: List<User>) {
@@ -28,16 +27,13 @@ class UserAdapter(val context: Context, private var users: List<User>) :
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val user = users[position]
         holder.binding.user = user
-        holder.binding.button3.setOnClickListener({
-            this
-                .onDeleteClick(user)
-        })
+        holder.binding.button3.setOnClickListener { this.onDeleteClick(user) }
     }
 
     override fun onDeleteClick(user: User) {
         SharedPref(context).deleteUser(user, object : UserListListener {
             override fun getUsers(users: List<User>) {
-                setUserList(users)
+                this@UserAdapter.setUserList(users)
             }
 
         })
@@ -49,5 +45,4 @@ interface DeleteButtonClickListener {
     fun onDeleteClick(user: User)
 }
 
-class MainViewHolder(val binding: AdapterUserBinding) : RecyclerView.ViewHolder(binding.root) {
-}
+class MainViewHolder(val binding: AdapterUserBinding) : RecyclerView.ViewHolder(binding.root)
